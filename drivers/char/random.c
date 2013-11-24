7546e66fcd1f27ed36587050b77c3f6f268dae28
Greg Price <price@mit.edu>
2013-11-23 22:41:02 -0800

    random: cut "limit" flag in favor of explicit tests
    The name corresponds to the test in account() -- we limit the
    data drawn from the input and /dev/random pools to our estimate
    of how much entropy they have -- but we also use it in places
    where it just happens to align with the logic we want.  All of
    these become clearer by just saying directly which pools get
    which logic.
    
    Signed-off-by: Greg Price <price@mit.edu>

--- drivers/char/random.c
+++ drivers/char/random.c
@@ -427,7 +427,6 @@ struct entropy_store {
 	int entropy_count;
 	int entropy_total;
 	unsigned int initialized:1;
-	unsigned int limit:1;
 	unsigned int last_data_init:1;
 	__u8 last_data[EXTRACT_SIZE];
 };
@@ -440,7 +439,6 @@ static __u32 nonblocking_pool_data[OUTPUT_POOL_WORDS];
 static struct entropy_store input_pool = {
 	.poolinfo = &poolinfo_table[0],
 	.name = "input",
-	.limit = 1,
 	.lock = __SPIN_LOCK_UNLOCKED(input_pool.lock),
 	.pool = input_pool_data
 };
@@ -448,7 +446,6 @@ static struct entropy_store input_pool = {
 static struct entropy_store blocking_pool = {
 	.poolinfo = &poolinfo_table[1],
 	.name = "blocking",
-	.limit = 1,
 	.pull = &input_pool,
 	.lock = __SPIN_LOCK_UNLOCKED(blocking_pool.lock),
 	.pool = blocking_pool_data,
@@ -898,7 +895,7 @@ static ssize_t extract_entropy(struct entropy_store *r, void *buf,
 static void _xfer_secondary_pool(struct entropy_store *r, size_t nbytes);
 static void xfer_secondary_pool(struct entropy_store *r, size_t nbytes)
 {
-	if (r->limit == 0 && r->initialized && random_min_urandom_seed) {
+	if (r == &nonblocking_pool && r->initialized && random_min_urandom_seed) {
 		unsigned long now = jiffies;
 
 		if (time_before(now,
@@ -929,7 +926,7 @@ static void account_xfer(struct entropy_store *dest, int nbytes,
 
 	/* Reserve some for /dev/random's pool, unless we really need it. */
 	*reserved_bytes = 0;
-	if (!dest->limit && dest->initialized) {
+	if (dest == &nonblocking_pool && dest->initialized) {
 		*reserved_bytes = 2 * (random_read_wakeup_bits / 8);
 	}
 }
@@ -985,8 +982,8 @@ retry:
 	entropy_count = orig = ACCESS_ONCE(r->entropy_count);
 	have_bytes = entropy_count >> (ENTROPY_SHIFT + 3);
 	ibytes = nbytes;
-	/* If limited, never pull more than available */
-	if (r->limit)
+	/* On the input pool and /dev/random, never pull more than available */
+	if (r != &nonblocking_pool)
 		ibytes = min_t(size_t, ibytes, have_bytes - reserved);
 	if (ibytes < min)
 		ibytes = 0;
