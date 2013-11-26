fd513ccd99467c83da20e9920a999744aedbc854
Greg Price <price@mit.edu>
2013-11-26 15:45:20 -0800

    random: convert extract_entropy to new output pool type

--- drivers/char/random.c
+++ drivers/char/random.c
@@ -435,8 +435,6 @@ struct entropy_store {
 	spinlock_t lock;
 	unsigned short add_ptr;
 	unsigned short input_rotate;
-	unsigned int last_data_init:1;
-	__u8 last_data[EXTRACT_SIZE];
 };
 
 static void push_to_pool(struct work_struct *work);
@@ -859,6 +857,11 @@ struct generator {
 	struct entropy_account *a;
 	struct entropy_store *_store;
 	const char *name;
+
+	/* Lock-protected data: */
+	spinlock_t lock;
+	unsigned int last_data_init:1;
+	__u8 last_data[EXTRACT_SIZE];
 };
 
 static struct entropy_store _blocking_pool = {
@@ -876,6 +879,7 @@ static struct generator blocking_pool = {
 	._store = &_blocking_pool,
 	.a = &_blocking_pool.a,
 	.name = "blocking",
+	.lock = __SPIN_LOCK_UNLOCKED(blocking_pool.lock),
 };
 
 static struct entropy_store _nonblocking_pool = {
@@ -893,6 +897,7 @@ static struct generator nonblocking_pool = {
 	._store = &_nonblocking_pool,
 	.a = &_nonblocking_pool.a,
 	.name = "nonblocking",
+	.lock = __SPIN_LOCK_UNLOCKED(nonblocking_pool.lock),
 };
 
 struct entropy_account *_nonblocking_account = &_nonblocking_pool.a;
@@ -1139,13 +1144,9 @@ static void extract_buf(struct entropy_store *r, __u8 *out)
 /*
  * This function extracts randomness from the "entropy pool", and
  * returns it in a buffer.
- *
- * The 'dest' parameter identifies the pool the entropy is to be used for,
- * or is NULL if it's not to be used in another pool.
  */
-static ssize_t extract_entropy(struct entropy_store *r, void *buf,
-			       size_t nbytes, struct entropy_account *dest,
-			       int *credit_bits)
+static ssize_t extract_entropy(struct generator *gen, void *buf,
+			       size_t nbytes)
 {
 	ssize_t ret = 0, i;
 	__u8 tmp[EXTRACT_SIZE];
@@ -1153,33 +1154,35 @@ static ssize_t extract_entropy(struct entropy_store *r, void *buf,
 
 	/* if last_data isn't primed, we need EXTRACT_SIZE extra bytes */
 	if (fips_enabled) {
-		spin_lock_irqsave(&r->lock, flags);
-		if (!r->last_data_init) {
-			r->last_data_init = 1;
-			spin_unlock_irqrestore(&r->lock, flags);
-			trace_extract_entropy(r->name, EXTRACT_SIZE,
-					      ENTROPY_BITS(r), _RET_IP_);
-			xfer_secondary_pool(r, EXTRACT_SIZE);
-			extract_buf(r, tmp);
-			spin_lock_irqsave(&r->lock, flags);
-			memcpy(r->last_data, tmp, EXTRACT_SIZE);
+		spin_lock_irqsave(&gen->lock, flags);
+		if (!gen->last_data_init) {
+			gen->last_data_init = 1;
+			spin_unlock_irqrestore(&gen->lock, flags);
+			trace_extract_entropy(gen->name, EXTRACT_SIZE,
+					      ENTROPY_BITS_A(gen->a), _RET_IP_);
+			xfer_secondary_pool(gen->_store, EXTRACT_SIZE);
+			extract_generator(gen, tmp);
+
+			spin_lock_irqsave(&gen->lock, flags);
+			memcpy(gen->last_data, tmp, EXTRACT_SIZE);
 		}
-		spin_unlock_irqrestore(&r->lock, flags);
+		spin_unlock_irqrestore(&gen->lock, flags);
 	}
 
-	trace_extract_entropy(r->name, nbytes, ENTROPY_BITS(r), _RET_IP_);
-	xfer_secondary_pool(r, nbytes);
-	nbytes = account(&r->a, nbytes, dest, credit_bits);
+	trace_extract_entropy(gen->name, nbytes,
+			      ENTROPY_BITS_A(gen->a), _RET_IP_);
+	xfer_secondary_pool(gen->_store, nbytes);
+	nbytes = account(gen->a, nbytes, NULL, NULL);
 
 	while (nbytes) {
-		extract_buf(r, tmp);
+		extract_generator(gen, tmp);
 
 		if (fips_enabled) {
-			spin_lock_irqsave(&r->lock, flags);
-			if (!memcmp(tmp, r->last_data, EXTRACT_SIZE))
+			spin_lock_irqsave(&gen->lock, flags);
+			if (!memcmp(tmp, gen->last_data, EXTRACT_SIZE))
 				panic("Hardware RNG duplicated output!\n");
-			memcpy(r->last_data, tmp, EXTRACT_SIZE);
-			spin_unlock_irqrestore(&r->lock, flags);
+			memcpy(gen->last_data, tmp, EXTRACT_SIZE);
+			spin_unlock_irqrestore(&gen->lock, flags);
 		}
 		i = min_t(int, nbytes, EXTRACT_SIZE);
 		memcpy(buf, tmp, i);
@@ -1280,7 +1283,7 @@ void get_random_bytes(void *buf, int nbytes)
 		       nonblocking_pool.a->entropy_total);
 #endif
 	trace_get_random_bytes(nbytes, _RET_IP_);
-	extract_entropy(&_nonblocking_pool, buf, nbytes, NULL, NULL);
+	extract_entropy(&nonblocking_pool, buf, nbytes);
 }
 EXPORT_SYMBOL(get_random_bytes);
 
@@ -1312,7 +1315,7 @@ void get_random_bytes_arch(void *buf, int nbytes)
 	}
 
 	if (nbytes)
-		extract_entropy(&_nonblocking_pool, p, nbytes, NULL, NULL);
+		extract_entropy(&nonblocking_pool, p, nbytes);
 }
 EXPORT_SYMBOL(get_random_bytes_arch);
 
