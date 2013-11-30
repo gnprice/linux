3fe7eaa8a0b6b6538b8bad96e93f5b85d8d10575
Greg Price <price@mit.edu>
2013-11-29 23:27:25 -0500

    wip skein api
    -rw-rw-r-- 1 greg greg 720 2013-11-29 01:56:02.978690023 -0500 include/linux/cryptohash.h

--- include/linux/cryptohash.h
+++ include/linux/cryptohash.h
@@ -1,12 +1,22 @@
 #ifndef __CRYPTOHASH_H
 #define __CRYPTOHASH_H
 
+#define SKEIN_STATE_WORDS 8
+#define SKEIN_CONTEXT_WORDS 10
+
+void skein_init(uint64_t *context);
+void skein_transform_notlast(uint64_t *context, const char *data);
+void skein_transform_last(uint64_t *context, const char *data);
+void skein_output_block(const uint64_t *state, size_t index, char *out);
+
+
 #define SHA_DIGEST_WORDS 5
 #define SHA_MESSAGE_BYTES (512 /*bits*/ / 8)
 #define SHA_WORKSPACE_WORDS 16
 
 void sha_init(__u32 *buf);
 void sha_transform(__u32 *digest, const char *data, __u32 *W);
+
 
 #define MD5_DIGEST_WORDS 4
 #define MD5_MESSAGE_BYTES 64
