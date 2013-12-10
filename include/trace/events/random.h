c24ce6c96d95fa34333e62fdb4601a73b12f0775
Greg Price <price@mit.edu>
2013-12-09 18:54:17 -0800

    random: count only catastrophic reseeds for initialization
    In the earlier commit "random: direct all routine input via input pool",
    we made sure that input comes in large reseeds which should be big
    enough to prevent an attacker from brute-forcing any one of them.
    This is important because a succession of small reseeds, if
    interspersed with output an attacker sees, could be brute-forced one
    by one.  Now update our accounting accordingly so that we track the
    largest single reseed, rather than the total of potentially small
    reseeds, and call ourselves initialized only with one large reseed.
    
    This shouldn't make much difference with the current code, as we
    don't make repeated small reseeds anyway, but it's best to be clear.
    
    Rename entropy_total to seed_entropy_bits to reflect its new function.
    
    While touching the not-yet-initialized warnings, checkpatch complains
    about printk(KERN_NOTICE, ...), so switch to pr_notice etc.
    
    Signed-off-by: Greg Price <price@mit.edu>

--- include/trace/events/random.h
+++ include/trace/events/random.h
@@ -61,17 +61,18 @@ DEFINE_EVENT(random__mix_pool_bytes, mix_pool_bytes_nolock,
 
 TRACE_EVENT(credit_entropy_bits,
 	TP_PROTO(const char *pool_name, int bits, int entropy_count,
-		 int entropy_since_push, int entropy_total, unsigned long IP),
+		 int entropy_since_push, int seed_entropy_bits,
+		 unsigned long IP),
 
 	TP_ARGS(pool_name, bits, entropy_count, entropy_since_push,
-		entropy_total, IP),
+		seed_entropy_bits, IP),
 
 	TP_STRUCT__entry(
 		__field( const char *,	pool_name		)
 		__field(	  int,	bits			)
 		__field(	  int,	entropy_count		)
 		__field(	  int,	entropy_since_push	)
-		__field(	  int,	entropy_total		)
+		__field(	  int,	seed_entropy_bits	)
 		__field(unsigned long,	IP			)
 	),
 
@@ -80,14 +81,14 @@ TRACE_EVENT(credit_entropy_bits,
 		__entry->bits			= bits;
 		__entry->entropy_count		= entropy_count;
 		__entry->entropy_since_push	= entropy_since_push;
-		__entry->entropy_total		= entropy_total;
+		__entry->seed_entropy_bits	= seed_entropy_bits;
 		__entry->IP			= IP;
 	),
 
 	TP_printk("%s pool: bits %d entropy_count %d entropy_since_push %d "
-		  "entropy_total %d caller %pF", __entry->pool_name,
+		  "seed_entropy_bits %d caller %pF", __entry->pool_name,
 		  __entry->bits, __entry->entropy_count,
-		  __entry->entropy_since_push, __entry->entropy_total,
+		  __entry->entropy_since_push, __entry->seed_entropy_bits,
 		  (void *)__entry->IP)
 );
 
